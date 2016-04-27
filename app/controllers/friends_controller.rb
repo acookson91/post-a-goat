374b556754d59d0282256ab666c99036cd8c077f
Simon Glancy <simonglancy@Simons-MacBook-Pro-2.local>
2016-04-27 11:55:47 +0100

    GoatBomb extracted

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -3,18 +3,13 @@ class FriendsController < ApplicationController
 
 before_action :authenticate_user!, :except => [:index]
   def index
-    @friends = Friend.all
+    current_user ? @friends = current_user.friends : @friends = []
   end
 
   def new
     @friend = Friend.new
   end
 
-  def show
-    @friend = Friend.find(params[:id])
-    GoatBomb.send_text(@friend)
-    redirect_to '/'
-  end
 
   def create
     @friend = current_user.friends.create(friend_params)
---
+++ app/controllers/goatbombs_controller.rb
@@ -0,0 +1,9 @@
+class GoatbombsController < ApplicationController
+
+  def create 
+    @friend = Friend.find(params[:friend_id])
+    GoatBomb.send_text(@friend)
+    redirect_to '/'
+  end
+
+end
