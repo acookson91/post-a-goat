7aa27ebe730375bee1a7ec12a27c10f9b41e111c
Shane Oston Stowe <shane@shaneoston.com>
2016-04-28 16:56:59 +0100

    added first protractor test file

---
+++ test/e2e/goatsFeatures.js
@@ -0,0 +1,32 @@
+describe('E2E: Main page', function() {
+
+  beforeEach(function() {
+    browser.get('/');
+  });
+
+  it('should load a home page', function() {
+    expect(element(by.css('#home')).isPresent()).toBeTruthy();
+  });
+
+  it('should load a register page', function() {
+    element(by.id("registerLink")).click();
+    expect(element(by.id('register')).isPresent()).toBeTruthy();
+  });
+
+  it('should allow a user to register',function() {
+    element(by.id("registerLink")).click();
+    var email = element(by.id('email'));
+    var username = element(by.css('input[type=text]'));
+    var password = element(by.css('input[type=password]'));
+
+    email.sendKeys('test@test.com');
+    username.sendKeys('tester');
+    password.sendKeys('test1234');
+
+    element(by.css('input[type=submit]')).click();
+
+    expect(element(by.id('welcome')).isPresent()).toBeTruthy();
+  });
+
+
+});
